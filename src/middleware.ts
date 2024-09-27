bee71e460371eaaf869acdce7de695a3241995f8
Arturo <ablrcomx@gmail.com>
2024-09-26 20:13:09 -0400

    Add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { NextRequest } from "next/server";
+import { withContext } from "./lib/middleware/pass-context";
+
+const allowedKeys: string[] = ["hello"]
+
+export default withContext(allowedKeys, (req, setContext) => {
+    
+});
