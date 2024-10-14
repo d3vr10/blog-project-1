cfcc4ab23851e839e207619ff4e4e59dde93ebe3
Arturo <ablrcomx@gmail.com>
2024-10-14 16:19:10 -0700

    Fix authentication schema

--- src/app/auth/_components/signin-form.tsx
+++ src/app/auth/_components/signin-form.tsx
@@ -3,18 +3,10 @@
 import {type SubmitHandler, useForm} from "react-hook-form";
 import {z} from "zod";
 import {zodResolver} from "@hookform/resolvers/zod"
-import {
-    Form,
-    FormField,
-    FormControl,
-    FormDescription,
-    FormItem,
-    FormLabel,
-    FormMessage
-} from "../../../components/ui/form";
+import {Form, FormControl, FormField, FormItem} from "../../../components/ui/form";
 import {Input} from "../../../components/ui/input";
 import {Button} from "../../../components/ui/button";
-import {GitHubIcon, GoogleIcon} from "../../../components/icons";
+import {GitHubIcon} from "../../../components/icons";
 import Image from "next/image"
 import Link from "next/link";
 import LoaderButton from "../../../components/loader-button";
@@ -22,30 +14,118 @@ import {signIn} from "@/lib/auth/actions";
 import {useToast} from "@/hooks/use-toast";
 import {useRouter} from "next/navigation";
 import {useAuth} from "./context";
-import {Dispatch, useState} from "react";
+import {Dispatch, useRef, useState} from "react";
+import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import {AlertCircle, AtSign, ChevronDown, KeyIcon, User} from "lucide-react";
+import {clsx} from "clsx";
 
 const authSchema = z.object({
     username: z.string().min(4),
+    email: z.string().email().optional(),
     password: z.string().min(8),
-})
+}).refine(data => data.email || data.password, {
+    message: "At least one of email or password must be supplied",
+    path: ["email", "password"], // Specify which fields to attach the error to
+});
 
 type AuthSchemaType = z.infer<typeof authSchema>
 
+type ResetMethod = "email" | "username"
+
+const ResetMethodIcon = ({method}: { method: ResetMethod }) => {
+    return method === "email" ? <AtSign className="h-4 w-4"/> : <User className="h-4 w-4"/>
+}
+const CustomKeyInput = ({
+                            signInMethod,
+                            setSignInMethod
+                        }: {
+    signInMethod: "username" | "email",
+    setSignInMethod: (value: "username" | "email") => void,
+}) => {
+    return (
+        <div className="relative">
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant="outline"
+                        className="absolute left-0 top-0 bottom-0 px-3 hover:bg-accent hover:text-accent-foreground"
+                    >
+                        <ResetMethodIcon method={signInMethod}/>
+                        <ChevronDown className="h-4 w-4 ml-2"/>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="start">
+                    <DropdownMenuItem onClick={() => setSignInMethod("username")}>
+                        <User className="h-4 w-4 mr-2"/>
+                        Username
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setSignInMethod("email")}>
+                        <AtSign className="h-4 w-4 mr-2"/>
+                        Email
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <FormField name={"email"} render={({field, fieldState}) => (
+                <FormItem className={clsx({
+                    "hidden": signInMethod === "username",
+                })}>
+                    <Input
+                        type={"email"}
+                        placeholder={"Enter your email"}
+                        className={`pl-20 ${fieldState.error ? "border-destructive" : ""}`}
+                        {...field}
+                    />
+                    {fieldState.error && (
+                        <p className="text-sm font-medium text-destructive flex items-center mt-2">
+                            <AlertCircle className="h-4 w-4 mr-2"/>
+                            {fieldState.error.message}
+                        </p>
+                    )}
+                </FormItem>
+            )}/>
+            <FormField name={"username"} render={({field, fieldState}) => (
+                <FormItem className={
+                    clsx({
+                        "hidden": signInMethod === "email"
+                    })
+                }>
+                    <Input
+                        type={"text"}
+                        placeholder={"Enter your username"}
+                        className={`pl-20 ${fieldState.error ? "border-destructive" : ""}`}
+                        {...field}
+                    />
+                    {fieldState.error && (
+                        <p className="text-sm font-medium text-destructive flex items-center mt-2">
+                            <AlertCircle className="h-4 w-4 mr-2"/>
+                            {fieldState.error.message}
+                        </p>
+                    )}
+                </FormItem>
+            )}/>
+        </div>
+    )
+}
+
 export default function SigninForm({setOpen}: { setOpen: Dispatch<any> }) {
+    const formRef = useRef<HTMLFormElement | null>(null)
     const {auth, setAuth} = useAuth()
     const [signInMethod, setSignInMethod] = useState<"username" | "email">("username")
     const router = useRouter()
     const {toast} = useToast()
+    const defaultValues = {
+        email: "",
+        username: "",
+        password: "",
+    }
     const form = useForm<AuthSchemaType>({
-        mode: "all", resolver: zodResolver(authSchema),
-        defaultValues: {
-            username: "",
-            password: ""
-        }
+        mode: "all",
+        resolver: zodResolver(authSchema),
+        defaultValues: defaultValues,
     })
-    const {formState: {errors}, formState} = form;
-    const onSubmit: SubmitHandler<AuthSchemaType> = async ({username, password}) => {
-        const response = await signIn({signInValue: username, signInMethod: signInMethod}, password)
+    const {resetField, formState: {errors}, formState} = form;
+    const onSubmit: SubmitHandler<AuthSchemaType> = async (values) => {
+        const response = await signIn({signInValue: values[signInMethod], signInMethod: signInMethod}, values.password)
         if (response.error) {
             let message = ""
             let title = ""
@@ -75,34 +155,52 @@ export default function SigninForm({setOpen}: { setOpen: Dispatch<any> }) {
     }
     return (
         <Form {...form}>
-            <form className="flex flex-col gap-y-4" onSubmit={form.handleSubmit(onSubmit)}>
-                <FormField
-                    name="username"
-                    render={({field, fieldState: {error}}) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input type="text" placeholder="eduardito123" {...field} />
-                            </FormControl>
-                            {error ? <FormMessage/> : <FormDescription>Tu nombre de usuario</FormDescription>}
+            <form className="flex flex-col gap-y-4" onSubmit={form.handleSubmit(onSubmit)} ref={formRef}>
+                <CustomKeyInput signInMethod={signInMethod} setSignInMethod={setSignInMethod}/>
+                <div className={"relative"}>
+                    <Button
+                        variant={"outline"}
+                        disabled
+                        className={"absolute left-0 bottom-0 top-0 px-[22px] text-primary"}
+                    >
+                        <KeyIcon className={"w-5 h-5 text-primary"}/>
+                    </Button>
+                    <FormField
+                        name="password"
+                        render={({field, fieldState: {error}}) => (
+                            <FormItem className={""}>
+                                <FormControl>
+                                    <Input type="password" className={"pl-20"} placeholder="*************" {...field} />
+                                </FormControl>
 
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    name="password"
-                    render={({field, fieldState: {error}}) => (
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                                <Input type="password" placeholder="*************" {...field} />
-                            </FormControl>
-                            {error ? <FormMessage/> : <FormDescription>Tu contraseña</FormDescription>}
+                                {error && (
+                                    <p className="text-sm font-medium text-destructive flex items-center mt-2">
+                                        <AlertCircle className="h-4 w-4 mr-2"/>
+                                        {error.message}
+                                    </p>
+                                )}
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <LoaderButton
+                    type={"button"}
+                    onClick={() => {
+                        const signInValue = form.getValues()[signInMethod]
+                        if (signInMethod === "username") {
+                            resetField("email", {
+                                defaultValue: defaultValues.email,
+                            })
+                        } else {
+                            resetField("username", {
+                                defaultValue: defaultValues.username,
+                            })
+                        }
 
-                        </FormItem>
-                    )}
+                        formRef.current?.requestSubmit()
+                    }}
+                    isSubmitting={formState.isSubmitting}
                 />
-                <LoaderButton isSubmitting={formState.isSubmitting}/>
 
                 {errors?.root ? <div className="text-sm text-red-800 text-center">{errors.root.message}</div> : ""}
 
